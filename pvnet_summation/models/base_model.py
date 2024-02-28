959ef75e51506842b4d0906753770b26b49ec42e
James Fulton <djamesfulton@yahoo.co.uk>
2024-02-28 14:57:11 +0000

    update to new pvnet

--- pvnet_summation/models/base_model.py
+++ pvnet_summation/models/base_model.py
@@ -96,9 +96,9 @@ class BaseModel(PVNetBaseModel):
     def pvnet_output_shape(self):
         """Return the expected shape of the PVNet outputs"""
         if self.pvnet_model.use_quantile_regression:
-            return (317, self.pvnet_model.forecast_len_30, len(self.pvnet_model.output_quantiles))
+            return (317, self.pvnet_model.forecast_len, len(self.pvnet_model.output_quantiles))
         else:
-            return (317, self.pvnet_model.forecast_len_30)
+            return (317, self.pvnet_model.forecast_len)
 
     def _training_accumulate_log(self, batch_idx, losses, y_hat, y, y_sum, times):
         """Internal function to accumulate training batches and log results.
