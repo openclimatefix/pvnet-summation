c7b77506b88e74d23c712ff86b3d2441e6d94d4a
James Fulton <djamesfulton@yahoo.co.uk>
2025-08-07 14:48:15 +0000

    fix tests

--- tests/models/test_base_model.py
+++ tests/models/test_base_model.py
@@ -1,6 +1,7 @@
 """Test the methods which stem from the BaseModel class"""
 from pvnet_summation.models import BaseModel
 import pvnet_summation.model_cards
+import yaml
 
 card_path = f"{pvnet_summation.model_cards.__path__[0]}/empty_model_card_template.md"
 
@@ -15,6 +16,12 @@ def test_save_load_model(tmp_path, model, raw_model_kwargs):
         **raw_model_kwargs,
     }
 
+    # Create a datamodule
+    datamodule_config_path = f"{tmp_path}/datamodule.yaml"
+    with open(datamodule_config_path, "w") as f:
+        datamodule = {"pvnet_model": {"model_id": "test", "revision": "abc"}}
+        yaml.dump(datamodule, f, sort_keys=False, default_flow_style=False)
+
     # Save the model
     model_output_dir = f"{tmp_path}/saved_model"
     model.save_pretrained(
@@ -22,6 +29,7 @@ def test_save_load_model(tmp_path, model, raw_model_kwargs):
         model_config=model_config,
         wandb_repo="test",
         wandb_id="abc",
+        datamodule_config_path=datamodule_config_path,
         card_template_path=card_path,
         push_to_hub=False,
     )
