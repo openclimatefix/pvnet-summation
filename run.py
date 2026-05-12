9e9a30608fa5a4ed1e01faf7840843665a8e2e3b
Felix <137530077+felix-e-h-p@users.noreply.github.com>
2026-05-12 11:26:21 +0100

    Add torch import and set matmul precision
    Import torch and set float32 matmul precision to medium.

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ import logging
 import sys
 
 import hydra
+import torch
 from omegaconf import DictConfig
 
 from pvnet_summation.training import train
@@ -15,7 +16,7 @@ from pvnet_summation.utils import maybe_apply_debug_mode, print_config
 
 logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
 
-
+torch.set_float32_matmul_precision('medium')
 
 @hydra.main(config_path="configs/", config_name="config.yaml", version_base="1.2")
 def main(config: DictConfig) -> None:
